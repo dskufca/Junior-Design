c61b9dd402d32970303d2447b72cf6ceed47f6f9
dskufca <danielle.skufca@tufts.edu>
2017-02-04 19:36:50 -0500

    Update driveSystem.ino

--- driveSystem.ino
+++ driveSystem.ino
@@ -3,7 +3,10 @@ int backward_1 = 3;
 int forward_2 = 5;
 int backward_2 = 6;
 
-int maxSpeed = 255;
+int drive_mode = 0;
+int previous_mode;
+
+int maxSpeed = 50;
 int currSpeed_1f = 0;
 int currSpeed_1b = 0;
 int currSpeed_2f = 0;
@@ -24,19 +27,96 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-    // accelerate forward
-    analogWrite(backward_1,currSpeed_1b);
-    analogWrite(backward_2,currSpeed_2b);
-    currentMillis = millis();
-    if (currentMillis-previousMillis > 100) {
-        if (currSpeed_1f < maxSpeed and currSpeed_2f < maxSpeed) {
-            currSpeed_1f = currSpeed_1f + 5;
-            currSpeed_2f = currSpeed_2f + 5;
-        }
+    // chose state of drive system
 
-        analogWrite(forward_1,currSpeed_1f);
-        analogWrite(forward_2,currSpeed_2f);
+    // adjust if in new state
+    if (drive_mode != previous_mode) {
+        switch (drive_mode) {
+            case 0:
+            {
+                // initialize off
+                
+                break;
+            }
+            case 1:
+            {
+                // initialize forward
+                currSpeed_1b = 0;
+                currSpeed_2b = 0;
+                analogWrite(backward_1,currSpeed_1b);
+                analogWrite(backward_2,currSpeed_2b);
+
+                break;
+            }
+            case 2:
+            {
+                // initialize backward
+
+                break;
+            }
+            case 3:
+            {
+                // initialize right
+
+                break;
+            }
+            case 4:
+            {
+                // initialize left
+
+                break;
+            }
+        }
     }
+    
+    // create a switch statement for the drive controls
+    switch (drive_mode) {
+        case 0:
+        {
+            // OFF
+
+            break;
+        }
+        case 1:
+        {
+            // FORWARD
+            // accelerate forward
+            currentMillis = millis();
+            if (currentMillis-previousMillis > 10) {
+                if (currSpeed_1f < maxSpeed) {
+                    currSpeed_1f = currSpeed_1f + 1;
+                }
+                if (currSpeed_2f < maxSpeed) {
+                    currSpeed_2f = currSpeed_2f + 1;
+                }
+                previousMillis = millis();
+
+                analogWrite(forward_1,currSpeed_1f);
+                analogWrite(forward_2,currSpeed_2f);
+            }
+            break;
+        }
+        case 2:
+        {
+            // BACKWARD
+            
+            break;
+        }
+        case 3:
+        {
+            // RIGHT
+
+            break;
+        }
+        case 4:
+        {
+            // LEFT
+
+            break;
+        }
+    }
+
+    previous_mode = drive_mode;
     
 }
 
